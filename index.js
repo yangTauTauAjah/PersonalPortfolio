2385364f5b1c2cbb845b6749f85d4ac0cfc7dc50
yangTauTauAjah <tesakun6464@gmail.com>
2023-01-26 23:45:12 +0700

    removing /public and moving all it's content to parent

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const PORT = 3000
 
-app.use('/', express.static('public'))
+app.use('/', express.static(__dirname))
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
